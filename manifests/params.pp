9c9083a24a0e69c81c9b57bf6e1032950e6f57b6
Jon Mosco <jonny.mosco@gmail.com>
2014-09-16 13:14:41 -0400

    Style guide changes.  Change version of PE supported to work on PE 3.x

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,7 @@
+# == Class: atomic::params
+#
 # Optional parameters in setting up Atomic Repo
+#
 class atomic::params {
   # Setting to 'absent' will fall back to the yum.conf
   # Settings here will be the default for all repos.
